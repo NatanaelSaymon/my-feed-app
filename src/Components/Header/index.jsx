e5b15a53f35a63308656db87a8f7aec89b5f2d56
Natanael Saymon <saymoneo@gmail.com>
2022-06-19 10:52:21 -0300

    :sparkles: feat: componente Header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,7 +1,10 @@
 import sytles from './styles.module.css';
+import igniteLogo from '../../assets/svg/ignite-icon.svg';
 
 export function Header() {
     return(
-        <div className={sytles.header}>Header</div>
+        <div className={sytles.header}>
+            <img src={igniteLogo} alt="Ignite Logo"/>
+        </div>
     )
 }

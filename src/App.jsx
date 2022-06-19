afabbd039818943d46ef5569c6d4505d506cdfb0
Natanael Saymon <saymoneo@gmail.com>
2022-06-19 10:52:49 -0300

    :lipstick: feat: mudanças gerais

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { Header } from "./Components/Header";
 import { Post } from "./Components/Post";
+import { Sidebar } from "./Components/Sidebar";
+import './global.css';
+import styles from './App.module.css';
 
 
 export function App() {
   return(
     <div>
       <Header/>
-      <Post />
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        
+        <main>
+          <Post 
+            author="Natanael Saymon"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+
+          <Post 
+            author="Suellen de Souza"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          />
+        </main>
+      </div>
     </div>
   )
 }
